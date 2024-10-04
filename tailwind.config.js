e622f205d14d7b5bfea61e63ea742bb8f64f6456
jbowwman8991 <jbowman8991@gmail.com>
2024-10-04 12:13:11 -0700

    adding customize

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ['./build/**/*.{html,js}'],
   theme: {
     extend: {
+      colors: {
+        customizing: {
+          pink: '#d594b7',
+          green: '#a6f1ac',
+          blue: '#8fb2d0',
+        }
+      },
       fontFamily: {
         "kalam": ['Kalam', 'cursive']
       }
