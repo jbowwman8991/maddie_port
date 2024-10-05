6fade01f3dc90c59444e1923ddbefd7e84079a64
jbowwman8991 <jbowman8991@gmail.com>
2024-10-04 22:24:39 -0700

    making mobile nav function

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,20 @@ module.exports = {
           pink: '#d594b7',
           green: '#a6f1ac',
           blue: '#8fb2d0',
-        }
+        },
       },
       fontFamily: {
-        "kalam": ['Kalam', 'cursive']
+        "kalam": ['Kalam', 'cursive'],
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' },
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
       }
     },
   },
