8b31b0f7153dddcb0a52b42276e7615c25da3b4b
jbowwman8991 <jbowman8991@gmail.com>
2024-10-04 22:44:16 -0700

    making theme circles toggle

--- build/js/main.js
+++ build/js/main.js
@@ -14,6 +14,87 @@ const initApp = () => {
 
     hamburgerBtn.addEventListener('click', toggleMenu)
     mobileMenu.addEventListener('click', toggleMenu)
+
+    const white = document.getElementById('white')
+    const pink = document.getElementById('pink')
+    const green = document.getElementById('green')
+    const blue = document.getElementById('blue')
+
+    const toggleWhiteTheme = () => {
+        white.classList.toggle('bg-slate-900')
+        white.classList.toggle('bg-slate-300')
+
+        if (pink.classList.contains('bg-slate-900')) {
+            pink.classList.toggle('bg-slate-900')
+            pink.classList.toggle('bg-slate-300')
+        }
+        if (green.classList.contains('bg-slate-900')) {
+            green.classList.toggle('bg-slate-900')
+            green.classList.toggle('bg-slate-300')
+        }
+        if (blue.classList.contains('bg-slate-900')) {
+            blue.classList.toggle('bg-slate-900')
+            blue.classList.toggle('bg-slate-300')
+        }
+    } 
+    white.addEventListener('click', toggleWhiteTheme)
+
+    const togglePinkTheme = () => {
+        pink.classList.toggle('bg-slate-900')
+        pink.classList.toggle('bg-slate-300')
+
+        if (white.classList.contains('bg-slate-900')) {
+            white.classList.toggle('bg-slate-900')
+            white.classList.toggle('bg-slate-300')
+        }
+        if (green.classList.contains('bg-slate-900')) {
+            green.classList.toggle('bg-slate-900')
+            green.classList.toggle('bg-slate-300')
+        }
+        if (blue.classList.contains('bg-slate-900')) {
+            blue.classList.toggle('bg-slate-900')
+            blue.classList.toggle('bg-slate-300')
+        }
+    } 
+    pink.addEventListener('click', togglePinkTheme)
+
+    const toggleGreenTheme = () => {
+        green.classList.toggle('bg-slate-900')
+        green.classList.toggle('bg-slate-300')
+
+        if (pink.classList.contains('bg-slate-900')) {
+            pink.classList.toggle('bg-slate-900')
+            pink.classList.toggle('bg-slate-300')
+        }
+        if (white.classList.contains('bg-slate-900')) {
+            white.classList.toggle('bg-slate-900')
+            white.classList.toggle('bg-slate-300')
+        }
+        if (blue.classList.contains('bg-slate-900')) {
+            blue.classList.toggle('bg-slate-900')
+            blue.classList.toggle('bg-slate-300')
+        }
+    } 
+    green.addEventListener('click', toggleGreenTheme)
+
+    const toggleBlueTheme = () => {
+        blue.classList.toggle('bg-slate-900')
+        blue.classList.toggle('bg-slate-300')
+
+        if (pink.classList.contains('bg-slate-900')) {
+            pink.classList.toggle('bg-slate-900')
+            pink.classList.toggle('bg-slate-300')
+        }
+        if (green.classList.contains('bg-slate-900')) {
+            green.classList.toggle('bg-slate-900')
+            green.classList.toggle('bg-slate-300')
+        }
+        if (white.classList.contains('bg-slate-900')) {
+            white.classList.toggle('bg-slate-900')
+            white.classList.toggle('bg-slate-300')
+        }
+    } 
+    blue.addEventListener('click', toggleBlueTheme)
 }
 
 document.addEventListener('DOMContentLoaded', initApp)
